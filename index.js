c3b838cdf34d97f7b5e9d1f5941bd443a147afad
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2022-08-23 13:39:31 -0300

    fix: cors issue

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+app.use('/posts', postRoutes);
+
 // https://www.mongodb.com/cloud/atlas
-const CONNECTION_URL = "mongodb+srv://app-user:app-user@my-private-cluster.n1yoq.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://app-user:app-user@my-private-cluster.n1yoq.mongodb.net/my-memories?retryWrites=true&w=majority"
 
 const PORT = process.env.PORT || 5000;
 
