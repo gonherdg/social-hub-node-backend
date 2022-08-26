d84b7e926894f7880f54b7a876bd9c50f56958a5
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2022-08-26 04:19:21 -0300

    feat: using dotenv

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-import express from 'express';
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from 'cors';
+import cors from "cors";
+import dotenv from "dotenv";
 
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
 
 // https://www.mongodb.com/cloud/atlas
-const CONNECTION_URL = "mongodb+srv://app-user:app-user@my-private-cluster.n1yoq.mongodb.net/my-memories?retryWrites=true&w=majority"
+const CONNECTION_URL =
+    "mongodb+srv://app-user:app-user@my-private-cluster.n1yoq.mongodb.net/my-memories?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-.catch((error) => console.log(error.message));
+mongoose
+    .connect(process.env.CONNECTION_URL)
+    .then(() =>
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    )
+    .catch((error) => console.log(error.message));
