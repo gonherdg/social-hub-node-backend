c46bef458212e26942d944cd7ebee66153dbca88
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2023-02-15 14:50:02 -0300

    take token with a middleware for communication with signed in user

--- controllers/posts.js
+++ controllers/posts.js
@@ -12,7 +12,11 @@ export const getPosts = async (req, res) => {
 
 export const createPost = async (req, res) => {
     const post = req.body;
-    const newPost = new PostMessage(post);
+    const newPost = new PostMessage({
+        ...post,
+        creator: req.userId,
+        createdAt: new Date().toISOString(),
+    });
 
     try {
         await newPost.save();
@@ -47,14 +51,26 @@ export const deletePost = async (req, res) => {
 
 export const likePost = async (req, res) => {
     const { id: _id } = req.params;
+
+    if (!req.userId) return res.json({ message: "Unauthenticated" });
+
     if (!mongoose.Types.ObjectId.isValid(_id))
         return res.status(404).send("No post with that id");
 
     const post = await PostMessage.findById(_id);
+
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+
+    if (index === -1) {
+        post.like.push(req.userId);
+    } else {
+        post.like = post.likes.filter((id) => id !== String(req.userId));
+    }
+
     const updatedPost = await PostMessage.findByIdAndUpdate(
         _id,
         {
-            likeCount: post.likeCount + 1,
+            post,
         },
         { new: true }
     );
